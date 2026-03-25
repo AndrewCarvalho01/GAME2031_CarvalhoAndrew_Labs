1372fd1eec48194b48c7ec340f0c3b3b2d9d7437
Andrew Carvalho <andrewcarvaho@gmail.com>
2026-03-25 09:36:48 -0400

    Lab 7 Complete

---
+++ GAME2031_Carvalho_Andrew_Labs/Assets/Scripts/CommandScripts/MoveRightCommand.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MoveRightCommand : ICommand
+{
+    private readonly CommandPlayer player;
+    public MoveRightCommand(CommandPlayer player)
+    {
+        this.player = player;
+    }
+    public void Execute()
+    {
+        player.MoveRight();
+    }
+
+    public void Undo()
+    {
+        player.MoveLeft();
+    }
+}
